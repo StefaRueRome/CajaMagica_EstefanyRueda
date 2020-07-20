417f4f92961a0cdae4e9e450f026082140c8b294
Estefany Rueda <yurley.rueda.2019@upb.edu.co>
2020-07-20 14:18:56 -0500

    Primer commit

---
+++ src/main/java/cajaMagica.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estefany Rueda
+ */
+public class cajaMagica {
+
+    int dimension = 3;
+    int[][] arreglo;
+
+    public cajaMagica() {
+        arreglo = new int[dimension][dimension];
+        crearCaja();
+    }
+
+    public void crearCaja() {
+        //encontrar la casilla central del primer renglon
+        int mitad = dimension / 2;
+        //ubicacion
+        int ubicacionColumna = mitad;
+        int ubicacionFila = 0;
+
+        //Asignar el 1 a la columna de la mitad de la primera fila
+        arreglo[ubicacionFila][ubicacionColumna] = 1;
+
+        //Guardar ubicación actual de la columna y fila
+        int ubicacionFActual = ubicacionFila;
+        int ubicacionCActual = ubicacionColumna;
+
+        //Recorrer los numeros desde 2
+        for (int i = 2; i < (Math.pow(arreglo.length, 2)); i++) {
+            ubicacionFila--;
+            ubicacionColumna--;
+            //cuando se devuelve se sale de la matriz
+            if (ubicacionFila < 0) {
+                ubicacionFila = arreglo.length - 1;
+            }
+            if (ubicacionColumna < 0) {
+                ubicacionColumna = arreglo.length - 1;
+            }
+            //Validación si la celda esta vacia 
+            if (arreglo[ubicacionFila][ubicacionColumna] == 0) {
+                arreglo[ubicacionFila][ubicacionColumna] = i;
+            } else {
+                // si la celda esta ocupada el numero se debe poner una fila mas abajo de la celda previa
+
+                ubicacionFila = ubicacionFActual + 1;
+                ubicacionColumna = ubicacionCActual;
+                arreglo[ubicacionFila][ubicacionColumna] = i;
+            }
+            //se vuelve a guardar la ubicacion actual
+            ubicacionFActual = ubicacionFila;
+            ubicacionCActual = ubicacionColumna;
+
+        }
+
+        mostrarArreglo();
+
+    }
+
+    public void mostrarArreglo() {
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = 0; j < arreglo.length; j++) {
+                System.out.println(arreglo[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        new cajaMagica();
+    }
+}
